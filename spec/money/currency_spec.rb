ac34e30891875948bd4a395bcd45eaa95542c1fc
Soham Chetan <sohamchetan@Sohams-MacBook-Pro.local>
2015-02-02 12:35:56 +0530

    added tests for >,>=,<,<= functionality

--- spec/money/currency_spec.rb
+++ spec/money/currency_spec.rb
@@ -17,44 +17,60 @@ describe 'Currency' do
     expect([money_paise, money_0, money_rupees].sort).to eq([money_0, money_paise, money_rupees])
   end
 
-describe 'addition' do
-  it 'addition' do
-    expect(money_paise + money_combo1).to eq(money_sum)
+  it 'return whether one money object is greater than another' do
+    expect(money_0 < money_combo1).to eq(true)
   end
 
-  it 'symmetric addition' do
-    expect(money_paise + money_combo1).to eq(money_combo1 + money_paise)
-  end
-  it 'associative addition' do
-    expect(money_paise + (money_combo1 + money_combo2)).to eq((money_paise + money_combo1) + money_combo2)
-  end
-end
-
-it 'subtraction' do
-  expect(money_sum - money_combo1).to eq(money_paise)
-end
-
-it 'should display as a string' do
-  expect(money_combo1.to_s).to eq("1 rupees 50 paise")
-end
-
-describe 'Equality' do
-  it 'should follow reflexivity' do
-    expect(money_paise).to eq(money_paise)
+  it 'return whether one money object is greater than or equal to another' do
+    expect(money_0 <= money_combo1).to eq(true)
   end
 
-  it 'should follow symmetry' do
-    expect(money_paise == money_combo1). to eq(money_combo1 == money_paise)
+  it 'return whether one money object is lesser than another' do
+    expect(money_0 > money_combo1).to eq(false)
   end
 
-  it 'should compare money object with nil' do
-    expect(money_paise).not_to eq(nil)
+  it 'return whether one money object is lesser than or equal to another' do
+    expect(money_0 >= money_combo1).to eq(false)
   end
 
-  it 'should compare money object with object of different Class' do
-    expect(money_paise).not_to eq(1)
+  describe 'addition' do
+    it 'addition' do
+      expect(money_paise + money_combo1).to eq(money_sum)
+    end
+
+    it 'symmetric addition' do
+      expect(money_paise + money_combo1).to eq(money_combo1 + money_paise)
+    end
+    it 'associative addition' do
+      expect(money_paise + (money_combo1 + money_combo2)).to eq((money_paise + money_combo1) + money_combo2)
+    end
   end
 
-end
+  it 'subtraction' do
+    expect(money_sum - money_combo1).to eq(money_paise)
+  end
+
+  it 'should display as a string' do
+    expect(money_combo1.to_s).to eq("1 rupees 50 paise")
+  end
+
+  describe 'Equality' do
+    it 'should follow reflexivity' do
+      expect(money_paise).to eq(money_paise)
+    end
+
+    it 'should follow symmetry' do
+      expect(money_paise == money_combo1). to eq(money_combo1 == money_paise)
+    end
+
+    it 'should compare money object with nil' do
+      expect(money_paise).not_to eq(nil)
+    end
+
+    it 'should compare money object with object of different Class' do
+      expect(money_paise).not_to eq(1)
+    end
+
+  end
 
 end
